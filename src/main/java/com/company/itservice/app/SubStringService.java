570204abaed82b51cdeef6d3f0dd98908c5be6f9
Viktor Glashchenko <vglaschenko@gmail.com>
2022-04-09 22:47:19 +0400

    itsrtvise-02-Улучшения кода

--- src/main/java/com/company/itservice/app/SubStringService.java
+++ src/main/java/com/company/itservice/app/SubStringService.java
@@ -1,48 +1,50 @@
 package com.company.itservice.app;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class SubStringService {
     public String calculate(String userSubString, String userTestedString) {
-        String[] subStrings = userStringToArrayStrings( userSubString );
-        String[] testedStrings = userStringToArrayStrings( userTestedString );
-        List<String> list = new ArrayList<>();
-        for (String subString : subStrings) if (isContains(subString, testedStrings)) list.add(subString);
+        ArrayList<String> arraySubStrings = userStringToArrayList(userSubString);
+        if(countChar(userSubString)%2 != 0 ) {
+            return "Error in input substrings: The number of \" characters must be even";
+        }
+
+        ArrayList<String> arrayTestedStrings = userStringToArrayList(userTestedString);
+        if(countChar(userTestedString)%2 != 0 ) {
+            return "Error in tested strings: The number of \" characters must be even";
+        }
+
+        SortedSet<String> stringSet = new TreeSet<>();
+        for(String subString : arraySubStrings)
+            if (isContains(subString, arrayTestedStrings)) stringSet.add(subString);
 
         String outString = "";
-        if( !list.isEmpty() ) {
-            Collections.sort(list);
+        if( !stringSet.isEmpty() ) {
+            List<String> list = new ArrayList<>(stringSet);
             StringBuilder sb = new StringBuilder();
-            sb.append(list.get(0));
-            for (int i = 1; i < list.size(); i++) sb.append(", ").append(list.get(i));
+            sb.append("\"").append(list.get(0)).append("\"");
+            for(int i = 1; i < list.size(); i++) sb.append(", \"").append(list.get(i)).append("\"");
             outString = sb.toString();
-        }
+            }
         return outString;
     }
 
-    private boolean isContains(String sub, String[] testedStrings) {
-        for (String testedString : testedStrings) if (testedString.contains(sub)) return true;
+    private boolean isContains(String sub, ArrayList<String> arrayTestedStrings) {
+        for(String testedString : arrayTestedStrings) if(testedString.contains(sub)) return true;
         return false;
     }
 
-    private String[] userStringToArrayStrings( String userString ) {
-        // Преобразуем строку подстрок в массив подстрок и удалим лишние символы "
-        String sub = seachReplaceSubString(userString);
-        String[] subStrs = sub.split("#");
-        for(int i = 0; i < subStrs.length; i++) {
-            subStrs[i] = subStrs[i].replace("\"", " ");
-            subStrs[i] = subStrs[i].trim();
-        }
-        return subStrs;
+    int countChar(String str) {
+        int counter = 0;
+        for (int i = 0; i < str.length(); i++)   if(str.charAt(i) == '"') counter++;
+        return counter;
     }
 
-    private String seachReplaceSubString(String s ) {
-        // Подстроки по условиям приведем к единообразию формата разделителей
-        for(String s2 : Arrays.asList(" ,", " ,", ", ", ","))  s = s.replace(s2,  "#");
-        return s;
+    private ArrayList<String> userStringToArrayList( String userString ) {
+        // Преобразуем строку из поля формы в массив подстрок
+        String[] userStrings = userString.split("\"");
+        ArrayList<String> arrayList = new ArrayList<>();
+        for(int i = 1; i < userStrings.length; i+=2 ) arrayList.add(userStrings[i]);
+        return arrayList;
     }
-
 }
